49a95a6cee4c2477cd31c6b2e008c4a09845e75b
codingHappiness <mstrong@ualberta.ca>
2016-06-02 11:52:42 +0000

    Enabled frei0r for distortion, and chromakey for only selecting edges

--- convert.sh
+++ convert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -i $1 -vf "split [main][edges]; [edges] edgedetect=low=0.1:high=0.4[out]; [main][out] blend=all_mode=lighten:all_opacity=0.9" output.mkv
+ffmpeg -i $1 -vf "split [main][edges]; [edges] edgedetect=low=0.1:high=0.4, chromakey=black, frei0r=distort0r:filter_params=0.05:0.01 [out]; [out][main] blend=all_opacity=0.5" output.mkv
