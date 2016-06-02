fed70e632cce26588b2fc4039af48738a8f8cdfb
codingHappiness <mstrong@ualberta.ca>
2016-06-02 13:04:30 +0000

    Chromakey is either too sensitive, or something is going wrong with overlaying the outline

--- convert.sh
+++ convert.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ffmpeg -i $1 -vf "split [main][edges]; [edges] edgedetect=low=0.1:high=0.4, chromakey=black, frei0r=distort0r:filter_params=0.05:0.01 [out]; [out][main] blend=all_opacity=0.5" output.mkv
+ffmpeg -i $1 -vf "split [main][edges]; 
+[edges] edgedetect=low=0.3:high=0.4, 
+frei0r=distort0r:filter_params=0.05:0.01:0.5:1,
+chromakey=0x000000 [out];
+[main][out] overlay" output.mkv
