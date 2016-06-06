999f47dfb285ca6984c2d13acd5d3559cb4b6d87
codingHappiness <mstrong@ualberta.ca>
2016-06-06 21:20:47 +0000

    Attempting dual overlays, currently not working as intended

--- convert.sh
+++ convert.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 #chromakey seems to be capturing the white too. Perhaps too thin?
 rm output.mkv
-ffmpeg -i $1 -vf "split [main][red]; 
-[red] edgedetect=low=0.3:high=0.4, 
-frei0r=distort0r:filter_params=0.05|0.01|0.5|1, 
+ffmpeg -i $1 -filter_complex "split [main][edges];
+[edges] edgedetect=low=0.3:high=0.4, 
+split=3[red][blue]; 
+[red] frei0r=distort0r:filter_params=0.05|0.01|0.5|1, 
 frei0r=colorize:filter_params=1|1|0, 
 colorkey=0x000000[red];
-[main][red] overlay " output.mkv
+[blue] frei0r=distort0r:filter_params=0.06|0.01|0.5|1, 
+frei0r=colorize:filter_params=0.55|1|0, 
+colorkey=0x000000[blue];
+[main][red] overlay [redmix];
+[redmix][blue] overlay" output.mkv
 #[edges] chromakey=0x000000:1 [out];
 #[blue] frei0r=colorize:filter_params=0.55|1|0 [blue];
 #split [main] [blue][green];
