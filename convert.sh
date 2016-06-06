b933b0c20f9154865deb1c412844a33bff223c6d
codingHappiness <mstrong@ualberta.ca>
2016-06-06 18:15:34 +0000

    Colorization is working

--- convert.sh
+++ convert.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-
-ffmpeg -i $1 -vf "split [main][edges]; 
-[edges] edgedetect=low=0.3:high=0.4, 
-frei0r=distort0r:filter_params=0.05:0.01:0.5:1,
-chromakey=0x000000 [out];
-[main][out] overlay" output.mkv
+#chromakey seems to be capturing the white too. Perhaps too thin?
+ffmpeg -i $1 -filter_complex "split [main][red]; 
+[red] edgedetect=low=0.3:high=0.4, 
+frei0r=distort0r:filter_params=0.05|0.01|0.5|1, 
+frei0r=colorize:filter_params=1|1|0 [red];
+[main][red] overlay [final]" -map "[final]" output.mkv
+#[edges] chromakey=0x000000:1 [out];
+#[blue] frei0r=colorize:filter_params=0.55|1|0 [blue];
+#split [main] [blue][green];
